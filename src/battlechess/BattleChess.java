9f7c05860cc85d6a7a2799d243990d2600c08204
Tim Sexton <tim2342@gmail.com>
2012-02-12 22:01:02 -0600

    chessboard actually displays now, using background image for the board

--- src/battlechess/BattleChess.java
+++ src/battlechess/BattleChess.java
@@ -5,8 +5,32 @@ import java.awt.event.*;
 import javax.swing.*;
  
 public class BattleChess {
+    static int[] window = {816,638};
+    int[] screen = {800,600};
     JPanel pages;
      
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                initGUI();
+            }
+        });
+    }
+
+    private static void initGUI() {
+        // Create and set up the window.
+        JFrame frame = new JFrame("BattleChess");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(window[0],window[1]);
+
+        // Create and set up the content pane.
+        BattleChess game = new BattleChess();
+        game.addOptions(frame.getContentPane());
+
+        //Display the window.
+        frame.setVisible(true);
+    }
+
     public void addOptions(Container pane) {
         // Create the title screen
         JPanel title = new JPanel();
@@ -15,7 +39,7 @@ public class BattleChess {
         JLabel titleScreen = new JLabel(new ImageIcon("./images/titlescreen.jpg"));
         titleScreen.setOpaque(true);
         title.add(titleScreen,-1);
-        titleScreen.setBounds(0,0,800,600);
+        titleScreen.setBounds(0,0,screen[0],screen[1]);
 
         JButton registerButton = new JButton("Register"); //new ImageIcon("./images/register.gif")
         registerButton.setBounds(180,350,200,25);
@@ -37,25 +61,33 @@ public class BattleChess {
 
         // Create the board
         JPanel board = new JPanel();
-        JLabel cells[] = new JLabel[64];
+        board.setLayout(null);
+
+        JLabel boardScreen = new JLabel(new ImageIcon("./images/boardscreen.jpg"));
+        boardScreen.setOpaque(true);
+        board.add(boardScreen,-1);
+        boardScreen.setBounds(0,0,screen[0],screen[1]);
+        
+        JPanel chessboard = new JPanel();
+        JLabel square[] = new JLabel[64];
         GridLayout grid = new GridLayout();
         grid.setRows(8);
-        grid.setHgap(3);
-        grid.setVgap(3);
+        grid.setHgap(1);
+        grid.setVgap(1);
         grid.setColumns(8);
-        board.setLayout(grid);
-        board.setSize(640,640);
+        chessboard.setOpaque(false);
+        chessboard.setLayout(grid);
+        chessboard.setBounds(150,50,498,498);
+        board.add(chessboard,0);
         
         int rowColor = 0;
         int i = 0;
         for (int x=0; x<=7; x++) {
             rowColor++;
             for (int y=0; y<=7; y++) {
-                cells[i] = new JLabel("", JLabel.CENTER);
-                cells[i].setOpaque(true);
-                if (rowColor % 2 == 0) cells[i].setBackground(Color.DARK_GRAY);
-                else cells[i].setBackground(Color.WHITE);
-                board.add(cells[i]);
+                square[i] = new JLabel("", JLabel.CENTER);
+                square[i].setOpaque(false); // add the cells but make them transparent
+                chessboard.add(square[i]);
                 rowColor++;
                 i++;
             }
@@ -65,29 +97,11 @@ public class BattleChess {
         pages = new JPanel(new CardLayout());
         pages.add(title, "titlescreen");
         pages.add(board, "chessboard");
+        // TODO: add the high score panel
+        // TODO: add the registration panel
+        // TODO: add the login panel
+        // TODO: add the credits panel
          
         pane.add(pages, BorderLayout.CENTER);
-    }
-     
-    private static void createAndShowGUI() {
-        // Create and set up the window.
-        JFrame frame = new JFrame("BattleChess");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,600);
-
-        // Create and set up the content pane.
-        BattleChess game = new BattleChess();
-        game.addOptions(frame.getContentPane());
-
-        //Display the window.
-        frame.setVisible(true);
-    }
-     
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
     }
 }
